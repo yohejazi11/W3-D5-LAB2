09c096bc80038d2ad6f61c4420d3af7245ea16a7
yohejazi11 <yohejazi11@gmail.com>
2024-09-12 13:27:07 +0300

    add project

---
+++ main.js
@@ -0,0 +1,44 @@
+const tvFrame=document.getElementById('tv-frame');
+
+
+
+function  updateTv(channel) {
+if(channel==="channel"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg');
+
+
+    }else if(channel==="channe2"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg');
+
+        
+    }else if(channel==="channe3"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg');
+
+        
+    }else if(channel==="channe4"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg');
+
+        
+    }else if(channel==="channe5"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg');
+
+        
+    }else if(channel==="channe6"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg');
+
+        
+    }else if(channel==="channe7"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg')
+
+        
+    }else if(channel==="channe8"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg')
+
+        
+    }else if(channel==="channe9"){
+        tvFrame.setAttribute('src','https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg')
+
+        
+    }
+    
+}
